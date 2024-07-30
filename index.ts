7cb587bb3f2626ae98bc40149e0caa1a645182dd
Habib Ur Rehman <madnihabib6@gmail.com>
2024-07-30 22:50:59 +0500

    This is calculator project.

---
+++ index.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+  {
+    name: "FirstNumber",
+    type: "number",
+    message: "Enter Your First Number: ",
+  },
+  {
+    name: "SecondNumber",
+    type: "number",
+    message: "Enter Your Second Number: ",
+  },
+  {
+    name: "Operator",
+    type: "list",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answers.Operator == "Addition") {
+  console.log(
+    `The addition of ${answers.FirstNumber} and ${answers.SecondNumber} gives: ${
+      answers.FirstNumber + answers.SecondNumber}`);
+} else if (answers.Operator == "Subtraction") {
+    console.log(
+      `The subtraction of ${answers.FirstNumber} and ${answers.SecondNumber} gives: ${
+        answers.FirstNumber - answers.SecondNumber}`);
+    } else if (answers.Operator == "Multiplication") {
+        console.log(
+          `The multiplication of ${answers.FirstNumber} and ${answers.SecondNumber} gives: ${
+            answers.FirstNumber * answers.SecondNumber}`);
+        } else {
+            console.log(
+              `The division of ${answers.FirstNumber} and ${answers.SecondNumber} is: ${
+                answers.FirstNumber / answers.SecondNumber}`);
+            }
